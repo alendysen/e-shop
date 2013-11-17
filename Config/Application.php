5a3e4114bdc029514bc251c2fbbf50df3c4bf79a
M. Saiqul Haq <saiqulhaq@gmail.com>
2013-11-17 09:32:14 +0700

    dynamic load environment configuration

--- Config/Application.php
+++ Config/Application.php
@@ -7,8 +7,37 @@ namespace Config;
 *
 * configurasi utama aplikasi
 */
+
 class Application
 {
-	const BASE_URL =  'http://local.himsiuin.org/';
-	static public $default_controller = 'Home';
+	static $config = array('default_controller' => 'Home');
+
+	public function __construct()
+	{
+		switch (ENVIRONMENT) {
+			case 'development':
+				self::$config = array_merge(self::$config, Environments\Development::$config);
+				break;
+			case 'testing':
+				self::$config = array_merge(self::$config, Environments\Testing::$config);
+				break;
+			case 'production':
+				self::$config = array_merge(self::$config, Environments\Production::$config);
+				break;
+		}
+	}
+
+	static public function set($property, $val){
+		self::$config[$property] = $val;
+	}
+
+	static public function __callStatic($name, $arg)
+	{
+		return self::$config[$name];
+	}
+
+	public function __call($name, $arg)
+	{
+		return self::$config[$name];
+	}
 }
