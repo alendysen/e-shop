f2e11146ac73fa5a279dc193d1d513fe79bd14dd
M. Saiqul Haq <saiqulhaq@gmail.com>
2013-11-17 09:30:56 +0700

    rename config/environment directory to namespaced dir

---
+++ Config/Environments/Development.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Config\Environments;
+/**
+*
+*/
+
+class Development
+{
+	static public $config = array(
+		'base_url' =>  'http://local.himsiuin.org/',
+		'database' => array(
+			'username' => 'saiqulhaq',
+			'password' => 'zxcvzxcv',
+			'database' => 'himsi_development'
+			)
+		);
+}
